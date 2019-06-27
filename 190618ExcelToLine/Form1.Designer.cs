72a777140c6acec6d51477665a8a0ecb980afc53
Hongwu Ran <742105493@qq.com>
2019-06-27 21:40:24 +0800

    添加项目文件。

---
+++ 190618ExcelToLine/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace _190618ExcelToLine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.axMapControl1 = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 42);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 118);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // axMapControl1
+            // 
+            this.axMapControl1.Location = new System.Drawing.Point(155, 12);
+            this.axMapControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.axMapControl1.Name = "axMapControl1";
+            this.axMapControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMapControl1.OcxState")));
+            this.axMapControl1.Size = new System.Drawing.Size(857, 639);
+            this.axMapControl1.TabIndex = 2;
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(12, 128);
+            this.axLicenseControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 675);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Controls.Add(this.axMapControl1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axMapControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private ESRI.ArcGIS.Controls.AxMapControl axMapControl1;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+    }
+}
+
---
+++ 190618ExcelToLine/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Aspose.Cells;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Display;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace _190618ExcelToLine
+{
+    public partial class Form1 : Form
+    {
+        Cells cells;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //修改路径
+            ReadExcel("C:\\Users\\rhw\\Desktop\\190618ExcelToLine\\190618ExcelToLine\\data\\各区域间出行分布.xls");
+            axMapControl1.Refresh();
+        }
+
+        //读取excel
+        private void ReadExcel(string path)
+        {
+            Workbook workbook = new Workbook(path);
+            cells = workbook.Worksheets[0].Cells;
+            for (int i = 1; i < cells.MaxDataRow + 1; i++)
+            {
+                Data2Line(cells[i, 3].DoubleValue, cells[i, 4].DoubleValue,
+                cells[i, 5].DoubleValue, cells[i, 6].DoubleValue);
+            }
+        }
+
+        //各行数据生成线
+        private void Data2Line(double lonF, double latF, double lonT, double latT)
+        {
+            IPoint point_From = new PointClass();
+            IPoint point_To = new PointClass();
+            IPolyline pPolyline = new PolylineClass();
+            point_From.PutCoords(lonF, latF);
+            point_To.PutCoords(lonT, latT);
+            pPolyline.FromPoint = point_From;
+            pPolyline.ToPoint = point_To;
+            ILayer layer =  this.axMapControl1.Map.Layer[0];
+            IFeatureLayer featureLayer = (IFeatureLayer)layer;
+            IFeature feature = featureLayer.FeatureClass.CreateFeature();
+            feature.Shape = pPolyline;
+            feature.Store();
+        }
+    }
+}
